34555d8bbfbf227b1f435c4c27dda7802208df3b
김혁수 <hyuksu0308@naver.com>
2025-10-15 10:40:46 +0900

    Feat: LoginPage/로고 추가

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,6 +1,7 @@
 import { LoginForm } from "@/features/user-login/ui/LoginForm";
 import "@/shared/api/interceptors";
 import styles from "./login.module.css";
+import Image from "next/image";
 import LoginImageSlider from "@/features/user-login/ui/LoginImageSlider";
 export default function LoginPage() {
   const images = ["Onboarding_1p", "Onboarding_2p", "Onboarding_3p"];
@@ -10,6 +11,13 @@ export default function LoginPage() {
         <LoginImageSlider images={images} />
       </div>
       <div className={styles.login_right}>
+        <Image
+          src="/images/dentrion_logo.svg"
+          alt="Dentrion Logo"
+          width={120}
+          height={24}
+          className={styles.logo}
+        />
         <LoginForm />
       </div>
     </div>
