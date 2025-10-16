1870645753624fd9b29087f9505b6789162dc82a
김혁수 <hyuksu0308@naver.com>
2025-10-16 17:41:22 +0900

    Feat: useStore persist 적용하여 sessionStorage에 데이터 저장

--- src/entities/user/model/userStore.ts
+++ src/entities/user/model/userStore.ts
@@ -1,15 +1,26 @@
 // src/entities/user/model/userStore.ts
 import { create } from "zustand";
 import { UserInfoResponse } from "./types";
-
+import { persist, createJSONStorage } from "zustand/middleware";
 interface UserState {
   userInfo: UserInfoResponse | null;
   setUserInfo: (user: UserInfoResponse) => void;
   clearUserInfo: () => void;
 }
 
-export const useUserStore = create<UserState>((set) => ({
-  userInfo: null,
-  setUserInfo: (user) => set({ userInfo: user }),
-  clearUserInfo: () => set({ userInfo: null }),
-}));
+export const useUserStore = create<UserState>()(
+  persist(
+    (set) => ({
+      userInfo: null,
+      setUserInfo: (user) => set({ userInfo: user }),
+      clearUserInfo: () => set({ userInfo: null }),
+    }),
+    {
+      name: "user-storage", // localStorage key
+      storage:
+        typeof window !== "undefined"
+          ? createJSONStorage(() => sessionStorage)
+          : undefined,
+    }
+  )
+);
