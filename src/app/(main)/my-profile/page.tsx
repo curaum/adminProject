3b3428c5ddb9e9743371b920f91a82c7ddc461bb
김혁수 <hyuksu0308@naver.com>
2025-10-23 18:21:50 +0900

    Feat: MyProfilePage/마이페이지 레이아웃 개발

--- src/app/(main)/my-profile/page.tsx
+++ src/app/(main)/my-profile/page.tsx
@@ -1,7 +1,28 @@
 "use client";
 
 import { useSearchParams, useParams } from "next/navigation";
-
+import { useUserStore } from "@/entities/user/model/userStore";
+import styles from "./MyProfile.module.css";
+import BottomNavBar from "@/shared/ui/BottomNavBar";
 export default function MyProfilePage() {
-  return <main>MY PROFILE PAGE</main>;
+  const userInfo = useUserStore((state) => state.userInfo);
+  return (
+    <main>
+      <div className={styles.profileRow}>
+        <div className={styles.nameCard}>성함</div>
+        <div>{userInfo?.userName}</div>
+      </div>
+      <BottomNavBar
+        buttons={[
+          {
+            id: "create",
+            label: "공지 등록",
+            onClick: () => handleSave(notice),
+            activeStyle: { backgroundColor: "#51c37e", color: "#fff" },
+            inactiveStyle: { backgroundColor: "#F5F7F7", color: "#7c7c7c" },
+          },
+        ]}
+      />
+    </main>
+  );
 }
