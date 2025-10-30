039d0a0060163ceff51e7c84a37b476e90d172db
큐라움 <curaum.sw.use@gmail.com>
2025-10-30 11:59:48 +0900

    Fix: middleware/accessToken 없으면 memoryToken 사용

--- src/middleware.ts
+++ src/middleware.ts
@@ -6,17 +6,19 @@ import type { NextRequest } from "next/server";
 export function middleware(req: NextRequest) {
   const { pathname } = req.nextUrl;
 
-  const token = req.cookies.get("accessToken")?.value;
+  const token =
+    req.cookies.get("accessToken")?.value ||
+    req.cookies.get("memoryToken")?.value;
 
   // 로그인 상태일 때 /login 접근 시 /notice로 리다이렉트
   if (pathname === "/login" && token) {
     return NextResponse.redirect(new URL("/notice", req.url));
   }
 
-  // // 로그인되지 않은 상태에서 /notice 접근 시 /login으로 리다이렉트
-  // if (pathname.startsWith("/notice") && !token) {
-  //   return NextResponse.redirect(new URL("/login", req.url));
-  // }
+  // 로그인되지 않은 상태에서 /notice 접근 시 /login으로 리다이렉트
+  if (pathname.startsWith("/notice") && !token) {
+    return NextResponse.redirect(new URL("/login", req.url));
+  }
 
   // 그 외는 그대로
   return NextResponse.next();
